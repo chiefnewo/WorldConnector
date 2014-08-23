3da9db0ce98275e1f7d9a3b3a091aa5adb7b33ac
chiefnewo <chiefnwo@tpg.com.au>
2014-08-24 01:02:38 +1000

    adding new enemy

---
+++ Assets/Scripts/EnemyPatroller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyPatroller : MonoBehaviour {
+
+	public float enemySpeed = 1.0f;
+
+	private List<Vector2> waypoints = new List<Vector2>();
+	private int targetWaypoint = 0;
+
+	// Use this for initialization
+	void Start () {
+		waypoints.Add(transform.position); // always have at least one waypoint, our starting point
+		Component[] childWaypoints = GetComponentsInChildren<Transform>();
+		foreach(Transform tfm in childWaypoints){
+			waypoints.Add (tfm.position);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// lerp to waypoint
+	}
+}
